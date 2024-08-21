7712d5ab8b2520158c529eaed9e7a73c656cb926
NatsusakiYomi <lanteng233@163.com>
2024-08-21 21:48:48 +0800

    v0.1.5-release_version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SciDB2Dataset
-+ 
++ requeirements
+    python=3.8
+    requests==2.23.0
+    datasets==2.7.1
+    Pillow==9.4.0
+    fsspec==2023.9.2
--- loadscienceDB.py
+++ loadscienceDB.py
@@ -8,8 +8,9 @@ import time
 
 os.environ["HF_DATA SETS_NUM_THREADS"] = "1"
 URL_TXT_ROOT = r'./urls'
-os.environ['HTTP_PROXY'] = 'http://127.0.0.1:49756'
-os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:49756'
+# 修改端口号
+os.environ['HTTP_PROXY'] = 'http://127.0.0.1:'
+os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:'
 
 
 def read_url_from_txt(path):
@@ -20,13 +21,16 @@ def read_url_from_txt(path):
 
 if __name__ == '__main__':
     OPTION='ALL'
+    # 选择数据集url
     PATH='7e0a4faa0d0649918ae3e94ef34b94af.txt'
     PATH='25d185f01b8c43b29420c48e50a11fad.txt'
     # PATH='8cf1ce3983ad4d5d8f45ee6469ab5dcc.txt'
     PATH="new.txt"
     REMOTE=True
     LOCAL_FILE_PATH='E:\Study\Postgrad_1\\research\\arrow\LocalDataset'
+    # LOADING SCRIPT路径
     LOCAL_SCRIPT_PATH="C:\\Users\\NatsusakiYomi\Documents\Study\Postgrad1\\research\Sci2DB\\test\SciDB2Dataset\scienceDBDatasetGenerator"
+    # 缓存路径
     CACHE_DIR='C:\\Users\\NatsusakiYomi\\Documents\\Study\\HF_cache_dir'
     if REMOTE:
         urls_all,file_extensions,url_split,file_structure=read_url_from_txt(path=PATH)
