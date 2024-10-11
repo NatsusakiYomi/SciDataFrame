576dd8424724214e662ee3d949aa7c8a6b34ac96
Yomi <lanteng233@163.com>
2024-10-11 18:09:55 +0800

    server data numerical analysis & preprocess(pending)

--- arrow_flight/client.py
+++ arrow_flight/client.py
@@ -2,36 +2,78 @@ import pyarrow as pa
 import pyarrow.flight as fl
 import model.DataFrame
 from model import DataFrame
+import pickle
 
 # 创建 FlightClient 实例
 client = fl.FlightClient('grpc://localhost:8815')
+start_train = False
+while not start_train:
+    dataset_id = input("Enter dataset id: ")+".txt"
 
-dataset_id = input("Enter dataset id: ")+".txt"
+    # 获取 schema
+    action = fl.Action("get_schema", dataset_id.encode("utf-8"))
+    schema_results = client.do_action(action)
+    schema=None
+    # 处理返回的 schema
+    for schema_bytes in schema_results:
+        schema = pa.ipc.open_file(schema_bytes.body).read_all()
+        print("Received Schema:", schema)
+        print("Received bytes:", schema.nbytes)
+        # 重建文件目录树
 
-# 获取 schema
-action = fl.Action("get_schema", dataset_id.encode("utf-8"))
-schema_results = client.do_action(action)
-schema=None
-# 处理返回的 schema
-for schema_bytes in schema_results:
-    schema = pa.ipc.open_file(schema_bytes.body).read_all()
-    print("Received Schema:", schema)
-    print("Received bytes:", schema.nbytes)
-    # 重建文件目录树
+    # 用户输入文件夹路径
+    folder_path = input("Enter folder path to retrieve files: ")
+    ticket = fl.Ticket(folder_path.encode('utf-8'))
+    df = DataFrame(schema=schema)
 
-# 用户输入文件夹路径
-folder_path = input("Enter folder path to retrieve files: ")
-ticket = fl.Ticket(folder_path.encode('utf-8'))
-df = DataFrame(schema=schema)
-if input("Streaming or not: ").lower() == 'yes':
-    client.do_action("streaming")
-    reader = client.do_get(ticket)
-    for index, chunk in enumerate(reader):
-        print(f'Row {index + 1} received: {chunk.data.nbytes} bytes')
-        df.concat(pa.Table.from_batches([chunk.data]))
-else:
-    # 使用 do_get 获取文件数据
-    reader = client.do_get(ticket)
-    read_table = reader.read_all()
-    df.concat(read_table)
-    print(f'{read_table.num_rows} rows received: {read_table.nbytes} bytes')
+    #进行数值特征分析
+    if input("Analyze num or not: ").lower() == 'yes':
+        action = fl.Action("numerical_analysis", folder_path.encode("utf-8"))
+        results = client.do_action(action)
+        result=None
+        # 处理返回的result
+        for result_bytes in results:
+            numerical_feature = pickle.loads(result_bytes.body)
+            print("Received Numerical features:", numerical_feature)
+            print("Received bytes:", result_bytes.body.size)
+
+    # 进行
+
+    if input("Streaming or not: ").lower() == 'yes':
+        client.do_action("streaming")
+        reader = client.do_get(ticket)
+        for index, chunk in enumerate(reader):
+            print(f'Row {index + 1} received: {chunk.data.nbytes} bytes')
+            df.concat(pa.Table.from_batches([chunk.data]))
+    else:
+        # 使用 do_get 获取文件数据
+        reader = client.do_get(ticket)
+        read_table = reader.read_all()
+        df.concat(read_table)
+        print(f'{read_table.num_rows} rows received: {read_table.nbytes} bytes')
+
+    start_train = input("Start train? ").lower() == 'yes'
+
+from lightfm import LightFM
+from lightfm.data import Dataset
+
+# 创建 LightFM 数据集
+dataset = Dataset()
+
+# 构建用户和产品特征
+dataset.fit((x for x in df['user_id']), (x for x in df['product_id']))
+
+# 将数据转换为交互矩阵
+(interactions, weights) = dataset.build_interactions(
+    ((row['user_id'], row['product_id'], row['interaction']) for _, row in df.iterrows())
+)
+
+# 训练 LightFM 模型（可以选择隐式或显式反馈）
+model = LightFM(loss='warp')  # 'warp' 适合隐式反馈数据
+model.fit(interactions, epochs=30, num_threads=2)
+
+# 模型预测
+user_id = 1
+scores = model.predict(user_id, np.arange(interactions.shape[1]))
+top_items = np.argsort(-scores)
+print("Recommended products for user:", top_items[:10])
--- arrow_flight/server.py
+++ arrow_flight/server.py
@@ -1,7 +1,13 @@
 import pyarrow as pa
 import pyarrow.flight as fl
+import pandas as pd
+import numpy as np
+import pickle
+
+from pandas.core.internals.construction import arrays_to_mgr
 
 from SciDBLoader import load_schema, load_scidb_dataset
+import ast
 
 SCHEMA = pa.schema([
     ('text', pa.string()),
@@ -16,7 +22,11 @@ class MyFlightServer(fl.FlightServerBase):
         self.dir_structure = None
         self.streaming = False
         self.numerical_analysis = False
-        self.recommendation_preprocess = False
+        self.preprocess = False
+        self.dataset_id = None
+        self.folder_path = None
+        # np.array格式的数据集
+        self.dataset = None
 
     def _make_flight_info(self, dataset):
         # TODO:每次请求创建一个flight_info
@@ -35,19 +45,26 @@ class MyFlightServer(fl.FlightServerBase):
     def do_action(self, context, action):
         if action.type == "get_schema":
             # 创建示例 schema
-            dataset_id = action.body.to_pybytes().decode('utf-8')
-            schema, self.dir_structure = load_schema(dataset_id)
+            self.dataset_id = action.body.to_pybytes().decode('utf-8')
+            schema, self.dir_structure = load_schema(self.dataset_id)
             sink = pa.BufferOutputStream()
             with pa.ipc.new_file(sink, schema.schema) as writer:
                 writer.write_table(schema)
             # 将 schema 序列化为 bytes
             return [sink.getvalue().to_pybytes()]
         elif action.type == "streaming":
+            if self.numerical_analysis:
+                raise fl.FlightError(f"Processing error: You can't stream the dataset after setting numerical analysis")
             self.streaming = True
         elif action.type == "numerical_analysis":
-            self.numerical_analysis = True
-        elif action.type == "recommendation_preprocess":
-            self.recommendation_preprocess = True
+            self.streaming, self.numerical_analysis = False, True
+            self.folder_path = action.body.to_pybytes().decode('utf-8')
+            self.get_dataset()
+            result = self.analyze_num()
+            return [result]
+        elif action.type == "preprocess":
+            self.preprocess = True
+
         else:
             raise NotImplementedError
 
@@ -57,24 +74,69 @@ class MyFlightServer(fl.FlightServerBase):
 
         # 读取文件并构造 RecordBatch
         # 这里假设读取的文件内容符合 Arrow 的表格式
-        dataset = load_scidb_dataset(self.dir_structure, dirs_string, streaming=self.streaming)
+        dataset = load_scidb_dataset(
+            self.dir_structure, dirs_string, streaming=not self.numerical_analysis and self.streaming)
 
         if self.numerical_analysis:
+            if self.streaming:
+                return Exception
 
-            pass
-        if self.recommendation_preprocess:
-            pass
+        if self.preprocess:
+            return fl.RecordBatchStream(self.nparray_to_table(self.dataset))
 
         def generate_batches():
             for example in iter(dataset):
                 table = pa.table(example)
                 yield table
 
+        print(dataset)
         if self.streaming:
             return fl.GeneratorStream(SCHEMA, generate_batches())
         else:
             # 返回 RecordBatchStream
             return fl.RecordBatchStream(dataset['train'].data.table)
+
+    def get_dataset(self):
+        dataset = load_scidb_dataset(self.dir_structure, self.folder_path, streaming=self.streaming)
+        df = dataset['train'].data.table.to_pandas()
+        print(df['text'][0][0])
+
+        def convert_to_numeric_matrix(data):
+            # 对每一行中的每个字符串解析成数值列表
+            print(data)
+            numeric_matrix = np.array([np.fromstring(item, sep=',', dtype=np.float64) for item in data])
+            return numeric_matrix
+
+        np_data_matrix = convert_to_numeric_matrix(ast.literal_eval(df['text'][0][0]))
+        self.dataset = np_data_matrix
+
+    def analyze_num(self):
+        np_data_matrix=self.dataset
+        column_stats = {
+            "row": np_data_matrix.shape[0],
+            "col": np_data_matrix.shape[1],
+            "max": np.max(np_data_matrix, axis=0),
+            "min": np.min(np_data_matrix, axis=0),
+            "mean": np.mean(np_data_matrix, axis=0),
+            "median": np.median(np_data_matrix, axis=0),
+            "variance": np.var(np_data_matrix, axis=0)
+        }
+        return pickle.dumps(column_stats)
+
+    def nparray_to_table(self,nparray):
+        # 生成新的 schema，每列顺序命名并指定类型为 float64
+        num_columns = nparray.shape[1]
+        fields = [pa.field(f"column{i + 1}", pa.float64()) for i in range(num_columns)]
+        new_schema = pa.schema(fields)
+
+        # 将 NumPy 数组的每一列转为 PyArrow Array，并生成 PyArrow Table
+        arrays = [pa.array(nparray[:, i], type=pa.float64()) for i in range(num_columns)]
+        restored_table = pa.Table.from_arrays(arrays, schema=new_schema)
+        return restored_table
+
+    def preprocess(self):
+        if not self.dataset:
+            self.get_dataset()
 
 
 if __name__ == "__main__":
